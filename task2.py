27ef6aa38f1739b0780f43db47f899dad747264c
olexii-d <tokyojapanjapan@gmail.com>
2026-02-20 00:27:24 +0900

    gen_num final

---
+++ task2.py
@@ -0,0 +1,31 @@
+import re
+from typing import Callable, Generator
+
+
+def generator_numbers(text: str) -> Generator[float, None, None]:
+    """
+    Генератор, що знаходить у тексті всі дійсні числа (float)
+    і повертає їх по одному через yield.
+    Числа мають бути відокремлені пробілом зліва,
+    а справа — пробілом, кінцем рядка або розділовим знаком.
+    """
+    pattern = r"(?<!\S)\d+\.\d+(?=(\s|$|[.,!?;:]))"
+
+    for match in re.finditer(pattern, text):
+        yield float(match.group(0))
+
+
+def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
+    """
+    Підсумовує всі числа, які повертає генератор func(text).
+    """
+    return sum(func(text))
+
+
+if __name__ == "__main__":
+    text = (
+        "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, "
+        "доповнений додатковими надходженнями 27.45 і 324.00 доларів."
+    )
+    total_income = sum_profit(text, generator_numbers)
+    print(f"Загальний дохід: {total_income}")

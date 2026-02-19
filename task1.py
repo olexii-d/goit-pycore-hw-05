57d2d1d0894f522d4e11bfdfce242d94479acdad
olexii-d <tokyojapanjapan@gmail.com>
2026-02-20 00:14:22 +0900

    fibonacci done

---
+++ task1.py
@@ -0,0 +1,24 @@
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+
+        # если уже считали — вернём из кеша
+        if n in cache:
+            return cache[n]
+
+        # считаем, сохраняем, возвращаем
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
+
+
+if __name__ == "__main__":
+    fib = caching_fibonacci()
+    print(fib(10))  # 55
+    print(fib(15))  # 610
